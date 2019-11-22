0f9115991ea5b976afb3656891438b0e72be7df6
Tomasz Barczyński <tomasz.barczynski@rspective.pl>
2019-11-22 12:40:08 +0100

    Remove sens info

--- server/server.js
+++ server/server.js
@@ -6,8 +6,8 @@ exports = {
             url: 'https://api.voucherify.io/v1/campaigns',
             method: 'GET',
             headers: {
-                'X-App-Id': 'f4d349de-751e-425b-b5c2-54705aa5e40b',
-                'X-App-Token': 'a3c8826e-f379-425d-8763-cd6d694c2fe2',
+                'X-App-Id': 'your-voucherify-api-id',
+                'X-App-Token': 'your-voucherify-api-key',
                 'Accept': 'application/json'
             }
         }, function(err, res, body) {
